5ad4b6c44103349e310aba3e2932b62b34c3cc83
ocircleo <salmanhossain11222626@gmail.com>
2023-11-29 18:46:41 +0600

    readme.md updated

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -2,7 +2,7 @@
 const Card = ({ data }) => {
     let { name, gender, phone, picture } = data
     return (
-        <div className=" w-fit gap-5 px-8 py-6 relative rounded outline outline-indigo-200 outline-offset-2  border flex flex-col sm:flex-row items-center justify-center">
+        <div className="lg:scale-125 w-fit gap-5 px-8 py-6 relative rounded outline outline-indigo-200 outline-offset-2  border flex flex-col sm:flex-row items-center justify-center">
             <div className="absolute z-0  h-full w-full left-0 top-0 blur-md shadow-indigo-100 shadow-md bg-gray-600"></div>
             <img src={picture?.large} alt={`profile picture of ${name?.first} ${name?.last}`} className=" z-10  h-32 w-32 select-none rounded-full" />
             <div className="flex flex-col z-10 text-white capitalize text-center sm:text-start">
--- src/note.js
+++ src/note.js
@@ -1,7 +1,8 @@
 /**
  * Internship Task 
- * Live Site Link: 
- * Github Code link: 
+ * My mail : web.salman.pro@gmail.com
+ * Live Site Link: https://basic-cards.netlify.app/
+ * Github Code link: https://github.com/ocircleo/basic-carrds
  * 
  * API Link: https://randomuser.me/api/?page=1&results=1&seed=abc
  * Design Link: https://cdn.discordapp.com/attachments/667463013443305512/1175943918941130833/Display_Profile.jpg
